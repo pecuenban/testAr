fa0265fcaf6a262a4c70f0a0053e6c896db5a216
Guillermo Boscá <guillermobosca.bro@gmail.com>
2021-03-10 10:27:16 +0100

    Subida del proyecto de Unity

---
+++ Assets/Main/Loading_Animation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading_Animation : MonoBehaviour
+{
+    float m_Speed;
+    
+    //Tiempo del temporizador 
+    float targetTime = 5.0f;
+
+    void Start()
+    {
+        //Velocidad de la rotacion
+        m_Speed = 38.0f;
+        transform.Rotate(60, 0, 60);
+    }
+
+    void Update()
+    {
+        //Rotacion del objeto
+        transform.Rotate(Vector3.up * m_Speed * Time.deltaTime, Space.Self);
+
+        //Temporizador para cambio de escena
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f){
+            timerEnded();
+        }
+    }
+    void timerEnded(){
+        SceneManager.LoadScene("Realidad_Aumentada");
+    }
+}
